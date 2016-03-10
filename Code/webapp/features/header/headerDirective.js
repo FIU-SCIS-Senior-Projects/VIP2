faad13c4e859c6ec4271ebf9372836b320fdb9ab
Steven <mrowe122@gmail.com>
2016-03-10 18:27:39 -0500

    removed uneeded controller

--- Code/webapp/features/header/headerDirective.js
+++ Code/webapp/features/header/headerDirective.js
@@ -1,16 +1,9 @@
 (function() {
     angular.module('vipHeader', [])
     .directive('vipHeader', function () {
-        var controller = function () {
-            var vm = this;
-        };
-
         return {
             templateUrl: 'features/header/headerTemplate.html',
-            restrict: 'E', // Can be used as HTML element or as an attribute
-            controller: controller,
-            controllerAs:'vm',
-            bindToController: true
+            restrict: 'E' // Can be used as HTML element or as an attribute
         };
 });
 }());
