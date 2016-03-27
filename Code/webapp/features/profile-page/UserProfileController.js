b838a0d7c3d91c93b0d2954fea76379045800e56
Steven <mrowe122@gmail.com>
2016-03-27 10:05:36 -0400

    added new fields

--- Code/webapp/features/profile-page/UserProfileController.js
+++ Code/webapp/features/profile-page/UserProfileController.js
@@ -5,19 +5,17 @@ angular.module('UserProfileController', [])
 		vm.mockData = [{
 			firstName: "Steven",
 			lastName: "Rowe",
-			email: "Rowe"
+			email: "mrowe009@fiu.edu"
 		}];
 
 		vm.message = "Test";
-
-		console.log(vm.mockData[0].firstName);
-
-
 		vm.editorEnabled = false;
 
 		vm.enableEditor = function() {
 			vm.editorEnabled = true;
-			vm.editableTitle = vm.mockData[0].firstName;
+			vm.firstName = vm.mockData[0].firstName;
+			vm.lastName = vm.mockData[0].lastName;
+			vm.email = vm.mockData[0].email;
 		};
 
 		vm.disableEditor = function() {
