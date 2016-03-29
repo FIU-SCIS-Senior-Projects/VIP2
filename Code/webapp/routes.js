e95d689cd9a17b9d0ca35ca54ad14a50b348ed0f
Steven <mrowe122@gmail.com>
2016-03-29 13:30:06 -0400

    spacing fix

--- Code/webapp/routes.js
+++ Code/webapp/routes.js
@@ -3,38 +3,38 @@ angular.module('routes', ['ui.router', 'UserProfileController'])
     .config(function($urlRouterProvider, $stateProvider, $locationProvider) {
         $urlRouterProvider.otherwise('/');
         $stateProvider
-            .state('contact', {
-                url:'/contact',
-                templateUrl: 'features/contact/contact.html'
-            })
-            .state('competitionInformation', {
-                url:'/competition-information',
-                templateUrl: 'features/competition/competition.html'
-            })
-            .state('home', {
-            	url:'/',
-            	templateUrl:'features/main-page/home.html'
-            })
-            .state('how-vip-credits-count', {
-                url:'/how-vip-credits-count',
-                templateUrl: 'features/how-vip-credits-count/index.html'
-            })
-            .state('evaluation', {
-                url:'/peer-evaluations',
-                templateUrl: 'features/evaluation-page/evaluation.html'
-            })
-            .state('graduateApplication', {
-                url:'/graduate-application',
-                templateUrl: 'features/graduate-application/graduateApplication.html'
-            })
-            .state('undergraduateApplication', {
-                url:'/undergraduate-application',
-                templateUrl: 'features/undergraduate-application/undergraduateApplication.html'
-            })
-            .state('userProfile', {
-                url:'/profile',
-                templateUrl: 'features/profile-page/userProfile.html',
-				controller: 'UserCtrl',
-				controllerAs: 'user'
-            });
+        .state('contact', {
+            url:'/contact',
+            templateUrl: 'features/contact/contact.html'
+        })
+        .state('competitionInformation', {
+            url:'/competition-information',
+            templateUrl: 'features/competition/competition.html'
+        })
+        .state('home', {
+            url:'/',
+            templateUrl:'features/main-page/home.html'
+        })
+        .state('how-vip-credits-count', {
+            url:'/how-vip-credits-count',
+            templateUrl: 'features/how-vip-credits-count/index.html'
+        })
+        .state('evaluation', {
+            url:'/peer-evaluations',
+            templateUrl: 'features/evaluation-page/evaluation.html'
+        })
+        .state('graduateApplication', {
+            url:'/graduate-application',
+            templateUrl: 'features/graduate-application/graduateApplication.html'
+        })
+        .state('undergraduateApplication', {
+            url:'/undergraduate-application',
+            templateUrl: 'features/undergraduate-application/undergraduateApplication.html'
+        })
+        .state('userProfile', {
+            url:'/profile',
+            templateUrl: 'features/profile-page/userProfile.html',
+            controller: 'UserCtrl',
+            controllerAs: 'user'
+        });
     });
