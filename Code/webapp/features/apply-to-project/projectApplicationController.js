85bf0b775e88807f0cd95ec5a7d956c2365145d0
mcond005 <Crazedbob4!>
2016-04-01 00:05:36 -0400

    student confirm info  finished

--- Code/webapp/features/apply-to-project/projectApplicationController.js
+++ Code/webapp/features/apply-to-project/projectApplicationController.js
@@ -7,8 +7,43 @@ angular
             firstName: "Miguel",
             lastName: "Conde",
             email: "mcond005@fiu.edu",
-            pID: "1234567"
+            pID: "1234567",
+            rank: "Senior",
+            gender: 'Male',
+            college: "'Engineering & Computing",
+            school: "School of Computing and Information Sciences",
+            semester: "Spring 2016"
         }];
+
+        vm.mockColleges = [
+            {
+                name: 'Business',
+                schools: [
+                    'School of Accounting',
+                    'Alvah H. Chapman Jr. Graduate School of Business',
+                    'R. Kirk Landon Undergraduate School of Business'
+                ]
+            },
+            {
+                name: 'Engineering & Computing',
+                schools: [
+                    'School of Computing and Information Sciences',
+                    'OHL School of Construction'
+                ]
+            },
+            {
+                name: 'Architecture + The Arts',
+                schools: [
+                    'Architecture',
+                    'Interior',
+                    'Landscape',
+                    'School of Music'
+                ]
+            }
+        ];
+
+        vm.genders = ['Male', 'Female'];
+        vm.semesters = ['Spring 2016', 'Summer 2016'];
 
         vm.mockProjects = [
             {
@@ -23,18 +58,25 @@ angular
             {
                 name: 'project 4'
             }
-
         ];
 
+        vm.ranks = ['Freshman', 'Sophomore', 'Junior', 'Senior', 'Post-Grad'];
+
         vm.selectedProject = vm.mockProjects[0];
+        vm.selectedCollege = vm.mockColleges[1];
         vm.message = "Test";
         vm.editorEnabled = false;
 
 
         vm.firstName = vm.mockData[0].firstName;
         vm.lastName = vm.mockData[0].lastName;
+        vm.gender = vm.mockData[0].gender;
         vm.email = vm.mockData[0].email;
         vm.pID= vm.mockData[0].pID;
+        vm.rank = vm.mockData[0].rank;
+        vm.school = vm.selectedCollege.schools[0];
+        vm.college = vm.mockData[0].college;
+        vm.semester = vm.mockData[0].semester;
 
 
         vm.save = function() {
@@ -42,7 +84,10 @@ angular
             vm.mockData[0].lastName = vm.lastName;
             vm.mockData[0].email = vm.email;
             vm.mockData[0].pID = vm.pID;
+            vm.mockData[0].rank = vm.rank;
+            vm.mockData[0].school = vm.school;
+            vm.mockData[0].college = vm.college;
 
-            console.log(vm.mockData[0].firstName);
+            console.log(vm.mockData[0].rank);
         };
     });
