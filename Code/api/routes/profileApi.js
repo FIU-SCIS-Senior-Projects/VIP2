e9fcb3d779c6ce4ee085cbf5afe734f035b84238
rvian003 <rvian003@fiu.edu>
2016-04-17 01:20:06 -0400

    Profile Update works fine, only updating the fields lastname, firstname, email fields, few things need to be taken into consideration after this point, password needs to be stored somehow from the google api to our DB if we want to use Local-Strategy to read from the DB the user, also not all the fields for the Profile are being stored and need to be added to the profile update endpoint afterwards

--- Code/api/routes/profileApi.js
+++ Code/api/routes/profileApi.js
@@ -9,13 +9,24 @@ module.exports = function(app, express) {
         .put(function (req, res) {
             console.log("This is the body");
             console.log(req.body);
-            Profile.update({ _id: req.body._id }, req.body);                
-            //     req.body, function (err) {
-            //     if(err) {
-            //         return res.send(err);
-            //     }
-            //     return res.json({success: true});
+            /*
+            * This update takes TOO LONG to complete therefore im using the not so short approach but the fastest of the two
+            */
+            // Profile.update({ _id: req.body._id }, req.body).then(function(WriteResult){
+            //     console.log(WriteResult);
+            //     return res.json(profile);
             // });
+            Profile.findById(req.body._id, function(err, profile){
+                profile.email = req.body.email;
+                profile.firstName = req.body.firstName;
+                profile.lastName = req.body.lastName;
+                //Missing fields go here
+                
+                profile.save(function(err){
+                    if(err) res.send(err);
+                    res.json(profile);
+                })
+            });
         })
         .get(function (req, res) {
             Profile.find({email:req.user.email}, function (err, profile) {
