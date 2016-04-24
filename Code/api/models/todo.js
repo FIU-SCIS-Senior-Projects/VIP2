0dd306b75bacf916be06a54564c163ebd9ccef77
Steven <mrowe122@gmail.com>
2016-04-24 11:51:34 -0400

    added link attribute for the link

--- Code/api/models/todo.js
+++ Code/api/models/todo.js
@@ -5,7 +5,8 @@ var ToDoSchema = new Schema({
     todo: {type: String, required: true},
     read: {type: Boolean, default: false},
     type: {type: String, required: true},
-    time: {type: Date, default: Date.now}
+    time: {type: Date, default: Date.now},
+    link: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Todo', ToDoSchema);
