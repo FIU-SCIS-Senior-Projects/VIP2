78538eeec5495f3b4160e2a514150447bca2a29e
tmoore <tmoor039@fiu.edu>
2016-05-06 17:31:58 -0400

    localdemo

--- Code/webapp/features/registration/userRegistrationController.js
+++ Code/webapp/features/registration/userRegistrationController.js
@@ -271,7 +271,7 @@ angular
                     vm.objectId = data.objectId;
                     vm.userData.recipient = vm.userData.email;
                     vm.userData.text = "Dear "+vm.userData.firstName +",\n\nWelcome to FIU's VIP Project!"+
-                       " Please verify your email with the link below and standby for your account to be verified by the PI.\n\n http://vip-dev.cis.fiu.edu/vip/verifyEmail/" + vm.objectId +"";
+                       " Please verify your email with the link below and standby for your account to be verified by the PI.\n\n http:localhost:3000/vip/verifyEmail/" + vm.objectId +"";
                     vm.userData.subject = "Welcome to FIU VIP Project!";
                     User.nodeEmail(vm.userData);
                     vm.message = data.message; // message returned by the API
@@ -281,7 +281,7 @@ angular
                     // send email to PI for approval
                     vm.userData.recipient2 = "sadjadi@cs.fiu.edu"; // NEED TO PUT MAIN PI EMAIL HERE FOR NOW
                     vm.userData.text2 = "Dear PI/CoPI,"+
-                        " A new user is attempting to register, please accept or reject using the following link:\n\ http://vip-dev.cis.fiu.edu/#/verifyuser/" + vm.objectId +"";
+                        " A new user is attempting to register, please accept or reject using the following link:\n\ http:localhost:3000/#/verifyuser/" + vm.objectId +"";
                     vm.userData.subject2 = "User Registration Request";
                     User.nodeEmail(vm.userData);
 
